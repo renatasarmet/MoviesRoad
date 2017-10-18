57fcb696ce0e809f066e2cfc6b1b51aa36a3abbd
renatasarmet <renatassmendes@hotmail.com>
2017-10-18 14:40:04 -0200

    Criando com projeto da Mari

---
+++ app/src/main/java/com/example/mariana/moviesroad/main_scene/MainView.java
@@ -0,0 +1,12 @@
+package com.example.mariana.moviesroad.main_scene;
+
+/**
+ * Created by mariana on 11/10/17.
+ */
+
+public interface MainView {
+    void setErrorUsername();
+    void setErrorPassword();
+    void efetuaLogin();
+}
+
